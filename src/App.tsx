dba93b543c77e59aff74784c8ba32eda05c7adae
cyberarrow <bcantilho@gmail.com>
2020-08-28 15:50:07 -0300

    feat: pagina de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
+import Routes from './routes';
 
 const App: React.FC = () => (
   <>
-    <h1>GoBarber Web</h1>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
     <GlobalStyle />
   </>
 );
