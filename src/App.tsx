e51dac1813fc675b4deceb6b12fd0e81dc1325e2
cyberarrow <bcantilho@gmail.com>
2020-09-03 19:01:16 -0300

    iniciando processo de autenticacao com a api

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 
+import { AuthProvider } from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
     <BrowserRouter>
