c910c465e6212383192e88099a4eccd953f7f5e8
cyberarrow <bcantilho@gmail.com>
2020-09-03 14:10:40 -0300

    conceitos de ux e utilizaçao de estados

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -21,7 +21,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber logo" />
 
-        <Form initialData={{ name: 'Beatriz'}} onSubmit={handleSubmit}>
+        <Form initialData={{ name: 'Beatriz' }} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
